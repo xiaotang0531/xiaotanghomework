83ee1237457d1bea339cb80947b353ea4e7917c8
[tangzhihua] <[904329390@qq.com]>
2020-06-13 12:02:10 +0800

    test git updata

--- python_实战一作业.py
+++ python_实战一作业.py
@@ -1,4 +1,4 @@
-
+#xiaotang test git
 #无返回值，有参数, 默认返回None
 def func1(name):
     print(name)
@@ -8,6 +8,11 @@ def func2():
     print("有返回值，无参数")
     return 1
 
+#有参数有返回值    
+def func3(name):
+    return name
+
 if __name__ == '__main__':
     func1("lili")
     func2()
+    func3("小唐")
